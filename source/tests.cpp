e5d1dad0d4a7722b4638700248225562c146499e
Vanessa Retz <vanessa.retz@aol.com>
2018-05-04 15:57:19 +0200

    ..

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,4 +1,5 @@
 #define CATCH_CONFIG_RUNNER
+#define _USE_MATH_DEFINES
 #include <catch.hpp>
 #include "vec2.hpp"
 
@@ -7,7 +8,54 @@ int main(int argc, char *argv[])
   return Catch::Session().run(argc, argv);
 }
 
-TEST_CASE("Vec2 tests"){
-  Vec2 a;
+TEST_CASE("Test cases for Vec2") {
+  Vec2 a = Vec2{};
+  Vec2 b = Vec2{5.0f, 2.0f};
+  Vec2 c = Vec2{2.0f, 1.0f};
 
+  SECTION("Empty constructor test") {
+    REQUIRE(a.x == 0.0f);
+    REQUIRE(a.y == 0.0f);
+  }
+
+  SECTION("Constructor test") {
+    REQUIRE(b.x == 5.0f);
+    REQUIRE(b.y == 2.0f);
+  }
+
+  SECTION("Operator += test") {
+    a += b;
+    b += c;
+    REQUIRE(b.x == 7.0f);
+    REQUIRE(b.y == 3.0f);
+    REQUIRE(a.x == 5.0f);
+    REQUIRE(a.y == 2.0f);
+  }
+
+  SECTION("Operator -= test") {
+    a -= b;
+    b -= c;
+    REQUIRE(b.x == 3.0f);
+    REQUIRE(b.y == 1.0f);
+    REQUIRE(a.x == -5.0f);
+    REQUIRE(a.y == -2.0f);
+  }
+
+  SECTION("Operator *= test") {
+    b *= 3.0f;
+    c *= 3.0f;
+    REQUIRE(b.x == 15.0f);
+    REQUIRE(b.y == 6.0f);
+    REQUIRE(c.x == 6.0f);
+    REQUIRE(c.y == 3.0f);
+  }
+
+  SECTION("Operator /= test") {
+    b /= 2;
+    c /= 2;
+    REQUIRE(b.x == 2.5f);
+    REQUIRE(b.y == 1.0f);
+    REQUIRE(c.x == 1.0f);
+    REQUIRE(c.y == 0.5f);
+  }
 }
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -2,10 +2,9 @@
 #define VEC2_CPP
 #include "vec2.hpp"
 #include <iostream>
-using namespace std ;
 
 
-Vec2::Vec2() :x(0.0), y(0.0) {};
+Vec2::Vec2() :x(0.0f), y(0.0f) {};
 
 Vec2::Vec2(float x, float y) : x(x), y(y) {};
 
@@ -24,7 +23,7 @@ Vec2& Vec2::operator-=(Vec2 const &v) {
 }
 
 Vec2& Vec2::operator/=(float s) {
-    if (s == 0.0) { cout << -1 ; }
+    if (s == 0.0f) { std::cout << -1 ; }
 
     else {
         x /= s;
@@ -38,3 +37,31 @@ Vec2& Vec2::operator*=(float s) {
     y*= s;
     return *this;
 }
+
+Vec2 operator+(Vec2 const& u, Vec2 const& v) {
+    return Vec2{ u.x + v.x, u.y + v.y};
+}
+
+Vec2 operator-(Vec2 const& u, Vec2 const& v) {
+    return Vec2{ u.x - v.x, u.y - v.y };
+}
+
+Vec2 operator*(Vec2 const& v, float s) {
+    return Vec2{ v.x * s, v.y * s };
+}
+
+Vec2 operator/(Vec2 const& v, float s){
+    if (s == 0.0f) {
+        std::cout << "Division by zero not allowed";
+        return Vec2{ v.x, v.y };
+    }
+    else {
+        return Vec2{ v.x / s, v.y / s };
+    }
+}
+
+Vec2 operator*(float s, Vec2 const& v) {
+    return Vec2{ v.x * s, v.y * s };
+}
+
+#endif
