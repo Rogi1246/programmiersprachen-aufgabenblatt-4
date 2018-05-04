640e97d4019a5e455f4a1f5740cb35f5311dc003
Vanessa Retz <vanessa.retz@aol.com>
2018-05-04 15:34:28 +0200

    tests changed

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,13 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
 
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
+}
+
+TEST_CASE("Vec2 tests"){
+  Vec2 a;
+
 }
