998a6fd9eb38269dc9bb0f0fa09fcb0e9d7afc8d
Vanessa Retz <vanessa.retz@aol.com>
2018-05-27 18:34:44 +0200

    edit task 2 & 3

--- source/aufgabe_2_und_3.cpp
+++ source/aufgabe_2_und_3.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
+#include <set>
 #include <list>
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -8,13 +10,35 @@ using namespace std;
 int main()
 {
     list<unsigned int> randomList;
+    set<unsigned int>  res;
+
+
+    cout << "random list :  \n";
+
     for(int i=0;i<=100;i++){
-        randomList.push_back(rand());
+        unsigned int random = rand()%100;
+        randomList.push_back(random);
+        cout << random << " . " ;
     }
 
-    for (list<unsigned int>::iterator it = randomList.begin(); it != randomList.end(); it++){
-    cout << *it;
+    cout << '\n' << "missing : " << '\n' ;
 
+    for(int i = 0; i<100; i++){
+        if(find(randomList.begin(), randomList.end(), i) == randomList.end()){
+            res.insert(i);
+        }
+    }
+
+    for(auto const& element : res){
+        cout << '\n' << element << " . ";
+    }
+
+    for(int i = 0; i<100; i++){
+        if(!(find(res.begin(), res.end(), i) != res.end())){
+
+            cout << '\n' << "frequency : " << '\n';
+            cout << i << " : " << count(randomList.begin(), randomList.end(),i) << '\n' ;
+        }
     }
 
     return 0;
